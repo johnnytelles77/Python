a1fb66b3b7c8c900b98b6abb70b1842660455996
johnnytelles77 <137516577+johnnytelles77@users.noreply.github.com>
2023-12-07 09:50:52 -0800

    primera clase

--- project/config/urls.py
+++ project/config/urls.py
@@ -1,10 +1,12 @@
 
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("core.urls")),
     path('cliente/', include("cliente.urls")),
     path('producto/', include("producto.urls")),
+    
 ]
